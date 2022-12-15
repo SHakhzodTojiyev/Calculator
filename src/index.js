ab08ab06a5a55a78a3e25767112a9c83e14c8b6c
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2022-12-15 18:15:03 +0500

    done

---
+++ src/index.js
@@ -0,0 +1,29 @@
+// console.log(window); // All HTML page
+window.addEventListener("load", () => {
+  // console.log("Finished...");
+  // document.querySelector("body").innerText = "Loading..."
+
+  // HTML DOM Elements
+  let resultBtn = document.getElementById("result-btn");
+  // Start JS coding
+  resultBtn.addEventListener("click", () => {
+    // HTML DOM Elements
+    let numberOne = document.getElementById("number_one").value;
+    let numberTwo = document.getElementById("number_two").value;
+    let expression = document.getElementById("expression").value;
+    let result = document.getElementById("result");
+
+    // console.log(typeof(numberOne)); // String
+    // console.log(typeof(numberTwo)); // String
+
+    numberOne = parseFloat(numberOne);
+    numberTwo = parseFloat(numberTwo);
+
+    console.log(numberOne);
+
+    // console.log(typeof(numberOne)); // Number
+    // console.log(typeof(numberTwo)); // Number
+
+    result.innerText = Function(`return ${numberOne} ${expression} ${numberTwo}`)();
+  });
+});
